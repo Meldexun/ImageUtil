e3b6a22de3271b9d20e6c67e8a133bb0f0c34953
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-02-05 20:17:06 +0100

    Rewrite BitInputStream to improve performance

--- src/main/java/meldexun/imageutil/gif/BitInputStream.java
+++ src/main/java/meldexun/imageutil/gif/BitInputStream.java
@@ -4,10 +4,14 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import meldexun.memoryutil.MemoryAccess;
+
 public class BitInputStream extends FilterInputStream {
 
-	private int current;
-	private int remaining;
+	private final byte[] buffer = new byte[512 + 4];
+	private final MemoryAccess bufferAccess = MemoryAccess.of(this.buffer);
+	private int index;
+	private int count;
 
 	public BitInputStream(InputStream in) {
 		super(in);
@@ -17,24 +21,18 @@ public class BitInputStream extends FilterInputStream {
 		if (bits < 1 || bits > 31) {
 			throw new IllegalArgumentException();
 		}
-		int result = 0;
-		int n = 0;
-		while (n < bits) {
-			if (this.remaining == 0) {
-				int b = this.read();
-				if (b < 0) {
-					return -1;
-				}
-				this.current = b;
-				this.remaining = 8;
+		if (this.count - this.index < bits) {
+			if (this.index > 8) {
+				System.arraycopy(this.buffer, this.index / 8, this.buffer, 0, this.count / 8 - this.index / 8);
+				this.count -= this.index & ~7;
+				this.index &= 7;
 			}
-			int count = Math.min(bits - n, this.remaining);
-			result |= (this.current & ((1 << count) - 1)) << n;
-			n += count;
-			this.current >>>= count;
-			this.remaining -= count;
+			this.count += this.in.read(this.buffer, this.count / 8, 512 - this.count / 8) * 8;
 		}
-		return result;
+		int rawData = this.bufferAccess.getInt(this.index / 8);
+		int data = (rawData >>> (this.index & 7)) & ((1 << bits) - 1);
+		this.index += bits;
+		return data;
 	}
 
 }
